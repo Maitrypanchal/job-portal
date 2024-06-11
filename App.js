ca183dafad19698ab9f51f476cced6fc451f8659
Maitrypanchal <maitrypanchal2@gmail.com>
2024-06-11 14:27:15 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import CompanyList from './CompanyList';
+
+const App = () => {
+  return (
+    <div className="App">
+      <CompanyList />
+    </div>
+  );
+};
+
+export default App;
